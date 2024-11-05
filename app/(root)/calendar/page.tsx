c7cacd7fd75738aeef32247188891f21d9958602
tung.lv <tung.lv@tinasoft.com.vn>
2024-11-05 22:58:05 +0700

    temporary

--- app/(root)/calendar/page.tsx
+++ app/(root)/calendar/page.tsx
@@ -1,5 +1,216 @@
-'use client';
+// 'use client';
 
+// import React, { useState, useEffect } from 'react';
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+// import moment from 'moment';
+// import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+// import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import '../css.css';
+// import { SearchBar } from '../components/searchBar';
+// import EventForm from '../components/eventForm';
+// import { newRequest } from '@/lib/newRequest';
+// import { headers } from 'next/headers';
+// import { useAppSelector } from '@/lib/redux/hooks';
+
+// moment.locale('en-GB');
+// const localizer = momentLocalizer(moment);
+// const DnDCalendar = withDragAndDrop(Calendar);
+// interface Event {
+//   title: string;
+//   start: Date;
+//   end: Date;
+//   description: string;
+//   location: string;
+//   participants: string;
+//   id?: string;
+// }
+// const CalendarSchedule = () => {
+//   const [events, setEvents] = useState<any>([]);
+//   console.log(events);
+
+//   const [isModify, setIsModify] = useState<boolean>(false);
+//   const [view, setView] = useState('month');
+//   const [date, setDate] = useState(new Date());
+//   const [showEventForm, setShowEventForm] = useState(false);
+//   const [newEvent, setNewEvent] = useState<Event>({
+//     title: '',
+//     start: new Date(),
+//     end: new Date(),
+//     description: '',
+//     location: '',
+//     participants: '',
+//   });
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [error, setError] = useState('');
+//   const token = useAppSelector((state: any) => state.auth.token);
+
+//   const transformToEvents = (apiData: any) => {
+//     return apiData.data.map((schedule: any) => ({
+//       id: schedule.id,
+//       title: schedule.title,
+//       description: schedule.description,
+//       location: schedule.room.id,
+//       participants: schedule.user.id,
+//       start: new Date(schedule.start_time),
+//       end: new Date(schedule.end_time),
+//     }));
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const res = await newRequest.get('/api/v1/schedule', {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+
+//         const events = transformToEvents(res.data);
+
+//         setEvents(events);
+//       } catch (error) {
+//         console.error('Error fetching events:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   const handleSelectSlot = (slotInfo: any) => {
+//     setNewEvent({
+//       title: '',
+//       start: slotInfo.start,
+//       end: slotInfo.end,
+//       description: '',
+//       location: '',
+//       participants: '',
+//     });
+//     setShowEventForm(true);
+//     setIsModify(false);
+//   };
+
+//   const handleSelectEvent = (event: any) => {
+//     setNewEvent({
+//       title: event.title,
+//       description: event.description,
+//       start: event.start,
+//       end: event.end,
+//       location: event.location,
+//       participants: event.participants,
+//       id: event.id,
+//     });
+//     setShowEventForm(true);
+//     setIsModify(true);
+//   };
+
+//   const handleEventDrop = ({ event, start, end }) => {
+//     const updatedEvents = events.map((ev) =>
+//       ev.id === event.id ? { ...ev, start, end } : ev,
+//     );
+//     setEvents(updatedEvents);
+//   };
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     setNewEvent({ ...newEvent, [name]: value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (!newEvent.title || !newEvent.start || !newEvent.end) {
+//       setError('Please fill in all required fields.');
+//       return;
+//     }
+//     if (newEvent.start >= newEvent.end) {
+//       setError('End time must be after start time.');
+//       return;
+//     }
+
+//     const newEventObject = { ...newEvent, id: events.length + 1 };
+
+//     setEvents((prevEvents) => [...prevEvents, newEventObject]);
+
+//     const res = await newRequest.post(
+//       '/api/v1/schedule/create',
+//       {
+//         location: parseInt(newEventObject.location),
+//         participants: parseInt(newEventObject.participants),
+//         description: newEventObject.description,
+//         start: newEventObject.start,
+//         end: newEventObject.end,
+//         title: newEventObject.title,
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       },
+//     );
+
+//     setShowEventForm(false);
+
+//     setNewEvent({
+//       title: '',
+//       start: new Date(),
+//       end: new Date(),
+//       description: '',
+//       location: '',
+//       participants: '',
+//     });
+
+//     setError('');
+//   };
+
+//   const filteredEvents = events.filter((event) =>
+//     event.description.toLowerCase().includes(searchTerm.toLowerCase()),
+//   );
+
+//   console.log(filteredEvents);
+
+//   return (
+//     <div>
+//       <div className="mb-6 flex justify-between items-center space-x-4">
+//         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+//       </div>
+
+//       <DnDCalendar
+//         localizer={localizer}
+//         events={filteredEvents}
+//         startAccessor="start"
+//         endAccessor="end"
+//         style={{ height: 600 }}
+//         onSelectSlot={handleSelectSlot}
+//         onSelectEvent={handleSelectEvent}
+//         onEventDrop={handleEventDrop}
+//         selectable
+//         resizable
+//         views={['day', 'week', 'month', 'agenda']}
+//         view={view}
+//         onView={setView}
+//         date={date}
+//         onNavigate={setDate}
+//         className="shadow-lg rounded-lg bg-white"
+//         tooltipAccessor={(event) => `${event.title}\n${event.description}`}
+//       />
+//       {showEventForm && (
+//         <EventForm
+//           isModify={isModify}
+//           newEvent={newEvent}
+//           handleInputChange={handleInputChange}
+//           handleSubmit={handleSubmit}
+//           setShowEventForm={setShowEventForm}
+//           error={error}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CalendarSchedule;
+
+"use client"
 import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -10,39 +221,30 @@ import '../css.css';
 import { SearchBar } from '../components/searchBar';
 import EventForm from '../components/eventForm';
 import { newRequest } from '@/lib/newRequest';
-import { headers } from 'next/headers';
 import { useAppSelector } from '@/lib/redux/hooks';
 
 moment.locale('en-GB');
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
-interface Event {
-  title: string;
-  start: Date;
-  end: Date;
-  description: string;
-  location: string;
-  participants: string;
-  id?: string;
-}
+
 const CalendarSchedule = () => {
   const [events, setEvents] = useState<any>([]);
   const [isModify, setIsModify] = useState<boolean>(false);
   const [view, setView] = useState('month');
   const [date, setDate] = useState(new Date());
   const [showEventForm, setShowEventForm] = useState(false);
-  const [newEvent, setNewEvent] = useState<Event>({
+  const [newEvent, setNewEvent] = useState({
     title: '',
     start: new Date(),
     end: new Date(),
     description: '',
     location: '',
     participants: '',
+    id: undefined,
   });
   const [searchTerm, setSearchTerm] = useState('');
   const [error, setError] = useState('');
   const token = useAppSelector((state: any) => state.auth.token);
-
 
   const transformToEvents = (apiData: any) => {
     return apiData.data.map((schedule: any) => ({
@@ -66,7 +268,6 @@ const CalendarSchedule = () => {
         });
 
         const events = transformToEvents(res.data);
-
         setEvents(events);
       } catch (error) {
         console.error('Error fetching events:', error);
@@ -74,7 +275,7 @@ const CalendarSchedule = () => {
     };
 
     fetchData();
-  }, []);
+  }, [token]);
 
   const handleSelectSlot = (slotInfo: any) => {
     setNewEvent({
@@ -84,6 +285,7 @@ const CalendarSchedule = () => {
       description: '',
       location: '',
       participants: '',
+      id: undefined,
     });
     setShowEventForm(true);
     setIsModify(false);
@@ -103,11 +305,28 @@ const CalendarSchedule = () => {
     setIsModify(true);
   };
 
-  const handleEventDrop = ({ event, start, end }) => {
-    const updatedEvents = events.map((ev) =>
-      ev.id === event.id ? { ...ev, start, end } : ev,
-    );
-    setEvents(updatedEvents);
+  const handleEventDrop = async ({ event, start, end }) => {
+    try {
+      const res = await newRequest.put(
+        `/api/v1/schedule/update/${event.id}`,
+        {
+          start: start,
+          end: end,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const updatedEvents = events.map((ev) =>
+        ev.id === event.id ? { ...ev, start, end } : ev,
+      );
+      setEvents(updatedEvents);
+    } catch (error) {
+      console.error('Error updating event:', error);
+    }
   };
 
   const handleInputChange = (e) => {
@@ -127,39 +346,49 @@ const CalendarSchedule = () => {
       return;
     }
 
-    const newEventObject = { ...newEvent, id: events.length + 1 };
-
-    setEvents((prevEvents) => [...prevEvents, newEventObject]);
-
-    const res = await newRequest.post(
-      '/api/v1/schedule/create',
-      {
-        location: parseInt(newEventObject.location),
-        participants: parseInt(newEventObject.participants),
-        description: newEventObject.description,
-        start: newEventObject.start,
-        end: newEventObject.end,
-        title: newEventObject.title,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
+    try {
+      const res = await newRequest.post(
+        '/api/v1/schedule/create',
+        {
+          location: parseInt(newEvent.location),
+          participants: parseInt(newEvent.participants),
+          description: newEvent.description,
+          start: newEvent.start,
+          end: newEvent.end,
+          title: newEvent.title,
         },
-      },
-    );
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
 
-    setShowEventForm(false);
+      const newEventData = {
+        id: res.data.data.id,
+        title: newEvent.title,
+        description: newEvent.description,
+        location: newEvent.location,
+        participants: newEvent.participants,
+        start: newEvent.start,
+        end: newEvent.end,
+      };
 
-    setNewEvent({
-      title: '',
-      start: new Date(),
-      end: new Date(),
-      description: '',
-      location: '',
-      participants: '',
-    });
-
-    setError('');
+      setEvents((prevEvents) => [...prevEvents, newEventData]);
+      setShowEventForm(false);
+      setNewEvent({
+        title: '',
+        start: new Date(),
+        end: new Date(),
+        description: '',
+        location: '',
+        participants: '',
+        id: undefined,
+      });
+      setError('');
+    } catch (error) {
+      console.error('Error creating event:', error);
+    }
   };
 
   const filteredEvents = events.filter((event) =>
@@ -199,6 +428,8 @@ const CalendarSchedule = () => {
           handleSubmit={handleSubmit}
           setShowEventForm={setShowEventForm}
           error={error}
+          events={events}
+          setEvents={setEvents}
         />
       )}
     </div>
--- app/(root)/components/eventForm.tsx
+++ app/(root)/components/eventForm.tsx
@@ -1,236 +1,19 @@
-// 'use client';
-
-// import React, { useEffect, useState } from 'react';
-// import moment from 'moment';
-// import { newRequest } from '@/lib/newRequest';
-// import { useAppSelector } from '@/lib/redux/hooks';
-
-// interface EventFormProps {
-//   newEvent: {
-//     title: string;
-//     start: Date;
-//     end: Date;
-//     description: string;
-//     location: string;
-//     participants: string[];
-//   };
-//   handleInputChange: (
-//     e: React.ChangeEvent<
-//       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-//     >,
-//   ) => void;
-//   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-//   setShowEventForm: (show: boolean) => void;
-//   error: string;
-//   isModify: boolean;
-// }
-
-// const EventForm: React.FC<EventFormProps> = ({
-//   newEvent,
-//   handleInputChange,
-//   handleSubmit,
-//   setShowEventForm,
-//   error,
-//   isModify,
-// }) => {
-//   const [users, setUsers] = useState([]);
-//   const token = useAppSelector((state: any) => state.auth.token);
-//   const [rooms, setRooms] = useState([]);
-
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       const res = await newRequest.get(`/api/v1/user?role=giangvien`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       setUsers(res.data.data);
-//     };
-
-//     const fetchRoom = async () => {
-//       const res = await newRequest.get(`/api/v1/room`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       setRooms(res.data.data);
-//     };
-
-//     fetchRoom();
-//     fetchUser();
-//   }, [token]);
-
-//   return (
-//     <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center">
-//       <form
-//         onSubmit={handleSubmit}
-//         className="bg-white p-8 rounded-lg shadow-xl w-full max-w-lg"
-//       >
-//         {isModify ? (
-//           <h2 className="text-2xl font-bold mb-4">Modify Event</h2>
-//         ) : (
-//           <h2 className="text-2xl font-bold mb-4">Add New Event</h2>
-//         )}
-
-//         {error && <p className="text-red-500 mb-4">{error}</p>}
-
-//         <div className="mb-4">
-//           <label htmlFor="title" className="block mb-2 font-bold">
-//             Title*
-//           </label>
-//           <input
-//             type="text"
-//             id="title"
-//             name="title"
-//             value={newEvent.title}
-//             onChange={handleInputChange}
-//             required
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           />
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="start" className="block mb-2 font-bold">
-//             Start Date and Time*
-//           </label>
-//           <input
-//             type="datetime-local"
-//             id="start"
-//             name="start"
-//             value={moment(newEvent.start).format('YYYY-MM-DDTHH:mm')}
-//             onChange={handleInputChange}
-//             required
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           />
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="end" className="block mb-2 font-bold">
-//             End Date and Time*
-//           </label>
-//           <input
-//             type="datetime-local"
-//             id="end"
-//             name="end"
-//             value={moment(newEvent.end).format('YYYY-MM-DDTHH:mm')}
-//             onChange={handleInputChange}
-//             required
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           />
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="description" className="block mb-2 font-bold">
-//             Description
-//           </label>
-//           <textarea
-//             id="description"
-//             name="description"
-//             value={newEvent.description}
-//             onChange={handleInputChange}
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           ></textarea>
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="location" className="block mb-2 font-bold">
-//             Location*
-//           </label>
-//           <select
-//             id="location"
-//             name="location"
-//             value={newEvent.location}
-//             onChange={handleInputChange}
-//             required
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           >
-//             <option value="">Select a room</option>
-//             {rooms.map((room: any) => (
-//               <option key={room.id} value={room.id}>
-//                 {room.name}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-
-//         <div className="mb-4">
-//           <label htmlFor="participants" className="block mb-2 font-bold">
-//             Assign to*
-//           </label>
-//           <select
-//             id="participants"
-//             name="participants"
-//             value={newEvent.participants}
-//             onChange={handleInputChange}
-//             required
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//           >
-//             <option value="">Select a user</option>
-//             {users.map((user: any) => (
-//               <option key={user.id} value={user.id}>
-//                 {user.full_name}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-
-//         <div className="flex justify-end space-x-2">
-//           <button
-//             type="button"
-//             onClick={() => setShowEventForm(false)}
-//             className="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
-//           >
-//             Cancel
-//           </button>
-
-//           {isModify && (
-//             <button
-//               type="button"
-//               onClick={handleDelete}
-//               className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"
-//             >
-//               Delete
-//             </button>
-//           )}
-
-//           {isModify ? (
-//             <button
-//               onClick={handleUpdate}
-//               className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//             >
-//               Update Event
-//             </button>
-//           ) : (
-//             <button
-//               type="submit"
-//               className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-//             >
-//               Save Event
-//             </button>
-//           )}
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default EventForm;
 'use client';
-
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { newRequest } from '@/lib/newRequest';
 import { useAppSelector } from '@/lib/redux/hooks';
+import toast from 'react-hot-toast';
 
 interface EventFormProps {
   newEvent: {
-    id?: string; // Thêm id để xác định sự kiện nếu cập nhật/xoá
+    id?: string;
     title: string;
     start: Date;
     end: Date;
     description: string;
     location: string;
-    participants: string[];
+    participants: string;
   };
   handleInputChange: (
     e: React.ChangeEvent<
@@ -241,6 +24,8 @@ interface EventFormProps {
   setShowEventForm: (show: boolean) => void;
   error: string;
   isModify: boolean;
+  events: any[];
+  setEvents: (events: any[]) => void;
 }
 
 const EventForm: React.FC<EventFormProps> = ({
@@ -250,6 +35,8 @@ const EventForm: React.FC<EventFormProps> = ({
   setShowEventForm,
   error,
   isModify,
+  events,
+  setEvents,
 }) => {
   const [users, setUsers] = useState([]);
   const [rooms, setRooms] = useState([]);
@@ -278,32 +65,71 @@ const EventForm: React.FC<EventFormProps> = ({
     fetchRoom();
   }, [token]);
 
-  // Hàm xử lý cập nhật sự kiện
+  // Handle update event
   const handleUpdate = async () => {
     try {
-      const res = await newRequest.put(`/api/v1/event/${newEvent.id}`, newEvent, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+      const res = await newRequest.put(
+        `/api/v1/schedule/update/${newEvent.id}`,
+        {
+          title: newEvent.title,
+          start: newEvent.start,
+          end: newEvent.end,
+          description: newEvent.description,
+          location: parseInt(newEvent.location),
+          participants: parseInt(newEvent.participants),
         },
-      });
-      alert('Event updated successfully!');
-      setShowEventForm(false); // Đóng form sau khi cập nhật thành công
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      // Update the events array with the modified event
+      if (Array.isArray(events)) {
+        const updatedEvents = [...events];
+        const eventIndex = updatedEvents.findIndex(
+          (event) => event.id === newEvent.id,
+        );
+        if (eventIndex !== -1) {
+          updatedEvents[eventIndex] = res.data.data;
+          setEvents(updatedEvents);
+        }
+      }
+
+      toast.success('Update successfully');
+      setShowEventForm(false);
     } catch (error) {
+      toast.error('Something went wrong');
       console.error('Error updating event:', error);
     }
   };
 
-  // Hàm xử lý xoá sự kiện
+  // Handle delete event
   const handleDelete = async () => {
     try {
-      await newRequest.delete(`/api/v1/event/${newEvent.id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      alert('Event deleted successfully!');
-      setShowEventForm(false); // Đóng form sau khi xoá thành công
+      if (newEvent.id) {
+        await newRequest.delete(`/api/v1/schedule/delete/${newEvent.id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        // Remove the deleted event from the events array
+        if (Array.isArray(events)) {
+          const updatedEvents = events.filter(
+            (event) => event.id !== newEvent.id,
+          );
+          setEvents(updatedEvents);
+        }
+
+        toast.success('Delete successfully');
+        setShowEventForm(false);
+      } else {
+        toast.error('Event ID is missing');
+      }
     } catch (error) {
+      toast.error('Something went wrong');
       console.error('Error deleting event:', error);
     }
   };
